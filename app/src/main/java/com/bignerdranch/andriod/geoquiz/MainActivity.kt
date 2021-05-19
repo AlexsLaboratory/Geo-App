f8613f317cfde892ffb98e96627b6ae6e7375c50
Alex Lowe <loweman64@outlook.com>
2021-05-19 13:24:06 -0400

    feat: Create Landscape Layout for APP
    This is the end of "Chapter 3".

--- app/src/main/java/com/bignerdranch/andriod/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/andriod/geoquiz/MainActivity.kt
@@ -2,9 +2,12 @@ package com.bignerdranch.andriod.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    Log.d(TAG, "onCreate(Bundle?) called")
     setContentView(R.layout.activity_main)
 
     trueButton = findViewById(R.id.true_button)
@@ -45,6 +49,31 @@ class MainActivity : AppCompatActivity() {
       updateQuestion()
     }
     updateQuestion()
+  }
+
+  override fun onStart() {
+    super.onStart()
+    Log.d(TAG, "onStart() called")
+  }
+
+  override fun onResume() {
+    super.onResume()
+    Log.d(TAG, "onResume() called")
+  }
+
+  override fun onPause() {
+    super.onPause()
+    Log.d(TAG, "onPause() called")
+  }
+
+  override fun onStop() {
+    super.onStop()
+    Log.d(TAG, "onStop() called")
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    Log.d(TAG, "onDestroy() called")
   }
 
   private fun updateQuestion() {
