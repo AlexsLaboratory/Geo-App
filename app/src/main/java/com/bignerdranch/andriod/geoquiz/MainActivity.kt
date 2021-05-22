dec253c060396cdc8212accaf5391307e609a2e3
Alex Lowe <loweman64@outlook.com>
2021-05-22 16:10:53 -0400

    feat: Allow Users to Cheat when Answering Questions
    When they cheat once it labels them a cheater for the rest of the game.
    This is the end of "Chapter 6".

--- app/src/main/java/com/bignerdranch/andriod/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/andriod/geoquiz/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.andriod.geoquiz
 
 import QuizViewModel
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,12 +12,14 @@ import androidx.lifecycle.ViewModelProvider
 
 private const val TAG = "MainActivity"
 private const val KEY_INDEX = "index"
+private const val REQUEST_CODE_CHEAT = 0
 
 class MainActivity : AppCompatActivity() {
 
   private lateinit var trueButton: Button
   private lateinit var falseButton: Button
   private lateinit var nextButton: Button
+  private lateinit var cheatButton: Button
   private lateinit var questionTextView: TextView
   private val quizViewModel: QuizViewModel by lazy {
     ViewModelProvider(this).get(QuizViewModel::class.java)
@@ -33,6 +36,7 @@ class MainActivity : AppCompatActivity() {
     trueButton = findViewById(R.id.true_button)
     falseButton = findViewById(R.id.false_button)
     nextButton = findViewById(R.id.next_button)
+    cheatButton = findViewById(R.id.cheat_button)
     questionTextView = findViewById(R.id.question_text_view)
 
     trueButton.setOnClickListener { view ->
@@ -47,6 +51,11 @@ class MainActivity : AppCompatActivity() {
       quizViewModel.moveToNext()
       updateQuestion()
     }
+
+    cheatButton.setOnClickListener {
+      startActivityForResult(CheatActivity.newIntent(this@MainActivity, quizViewModel.currentQuestionAnswer), REQUEST_CODE_CHEAT)
+    }
+
     updateQuestion()
   }
 
@@ -68,7 +77,7 @@ class MainActivity : AppCompatActivity() {
   override fun onSaveInstanceState(savedInstanceState: Bundle) {
     super.onSaveInstanceState(savedInstanceState)
     Log.i(TAG, "onSaveInstanceState")
-    savedInstanceState.putInt(KEY_INDEX,  quizViewModel.currentIndex)
+    savedInstanceState.putInt(KEY_INDEX, quizViewModel.currentIndex)
   }
 
   override fun onStop() {
@@ -81,6 +90,17 @@ class MainActivity : AppCompatActivity() {
     Log.d(TAG, "onDestroy() called")
   }
 
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    if (resultCode != RESULT_OK) {
+      return
+    }
+
+    if (requestCode == REQUEST_CODE_CHEAT) {
+      quizViewModel.isCheater = data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+    }
+  }
+
   private fun updateQuestion() {
     val questionTextResId = quizViewModel.currentQuestionText
     questionTextView.setText(questionTextResId)
@@ -88,10 +108,10 @@ class MainActivity : AppCompatActivity() {
 
   private fun checkAnswer(userAnswer: Boolean) {
     val correctAnswer = quizViewModel.currentQuestionAnswer
-    val messageResId = if (userAnswer == correctAnswer) {
-      R.string.correct_toast
-    } else {
-      R.string.incorrect_toast
+    val messageResId = when {
+      quizViewModel.isCheater -> R.string.judgment_toast
+      userAnswer == correctAnswer -> R.string.correct_toast
+      else -> R.string.incorrect_toast
     }
     Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
   }
