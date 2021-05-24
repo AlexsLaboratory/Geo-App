02cb3e3953cd71e8fde0f387d53862b3a07c8279
Alex Lowe <loweman64@outlook.com>
2021-05-24 16:08:51 -0400

    feat: Prevent Users From Clearing Out Cheating Result by Rotating the Device

--- app/src/main/java/com/bignerdranch/andriod/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/andriod/geoquiz/CheatActivity.kt
@@ -5,20 +5,29 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 private const val EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true"
 const val EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown"
+private const val KEY_HAS_CHEATED = "cheated"
 
 class CheatActivity : AppCompatActivity() {
   private lateinit var answerTextView: TextView
   private lateinit var showAnswerButton: Button
   private var answerIsTrue = false
 
+  private val cheatViewModel: CheatViewModel by lazy {
+    ViewModelProvider(this).get(CheatViewModel::class.java)
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_cheat)
+    val isCheater = savedInstanceState?.getBoolean(KEY_HAS_CHEATED, false) ?: false
+    cheatViewModel.isCheater = isCheater
     answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
     answerTextView = findViewById(R.id.answer_text_view)
     showAnswerButton = findViewById(R.id.show_answer_button)
@@ -28,8 +37,18 @@ class CheatActivity : AppCompatActivity() {
         else -> R.string.false_button
       }
       answerTextView.setText(answerText)
-      setAnswerShownResult(true)
+      setAnswerShownResult()
+      cheatViewModel.isCheater = true
     }
+
+    if (cheatViewModel.isCheater) {
+      setAnswerShownResult()
+    }
+  }
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    outState.putBoolean(KEY_HAS_CHEATED, cheatViewModel.isCheater)
   }
 
   companion object {
@@ -40,9 +59,9 @@ class CheatActivity : AppCompatActivity() {
     }
   }
 
-  private fun setAnswerShownResult(isAnswerShown: Boolean) {
+  private fun setAnswerShownResult() {
     val data = Intent().apply {
-      putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+      putExtra(EXTRA_ANSWER_SHOWN, cheatViewModel.isCheater)
     }
     setResult(Activity.RESULT_OK, data)
   }
---
+++ app/src/main/java/com/bignerdranch/andriod/geoquiz/CheatViewModel.kt
@@ -0,0 +1,7 @@
+package com.bignerdranch.andriod.geoquiz
+
+import androidx.lifecycle.ViewModel
+
+class CheatViewModel : ViewModel() {
+  var isCheater = false
+}
